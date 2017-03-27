e2b1cd751715dbd00859d734c973330ccbff2032
Mirza Joldic <mirza.joldic@gmail.com>
2017-03-27 14:34:28 -0400

    add Grid Item numbers

--- src/GridItem.js
+++ src/GridItem.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import TextArea from './TextArea';
 
 const Item = styled.div`
+  position: relative;
   transition: all 300ms ease-in-out;
   padding: .5rem;
   > textarea {
@@ -12,15 +13,40 @@ const Item = styled.div`
   }
   &:hover {
     outline: 1px dashed lightgray;
-    >textarea {
+    > textarea {
       height: 100%;
       width: 100%;
       transition-delay: 0s;
       opacity: 1;
     }
   }
+  &:hover > div {
+    opacity: 1;
+    transition-delay: 0s;
+  }
   ${props => props.globalItemStyle}
   ${props => props.itemStyle}
+`;
+
+const ItemNumberContainer = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  pointer-events: none;
+  transition: all 300ms ease-in-out;
+  transition-delay: 500ms;
+  opacity: ${props => props.autoHide ? 0 : 1};
+`;
+
+const ItemNumber = styled.div`
+  font-size: 2em;
+  color: white;
+  opacity: .5;
 `;
 
 class GridItem extends React.Component {
@@ -35,10 +61,12 @@ class GridItem extends React.Component {
         <TextArea
           value={this.props.itemStyle}
           onChange={this.updateItemStyle}/>
+        <ItemNumberContainer autoHide={this.props.autoHide}>
+          <ItemNumber>{this.props.index + 1}</ItemNumber>
+        </ItemNumberContainer>
       </Item>
     );
   }
-
 }
 
 export default GridItem;
